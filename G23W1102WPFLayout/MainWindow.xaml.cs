0ec0cc8ecc1745f25885396201693275b2ab7785
BWOH <bwoh@outlook.kr>
2023-11-15 04:54:38 +0900

    초기 커밋.

---
+++ G23W1102WPFLayout/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace G23W1102WPFLayout {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        //private void OnButton(object sender, RoutedEventArgs e) {
+        //    MyLabel.Content = "오랜 만입니다.";
+        //}
+    }
+}
